45e288a111505c20c90498cd8065e44f5d8cf50a
jowettgo <jowettgo@gmail.com>
2015-01-23 10:41:08 -0800

    Update get_all_prime.php
    set_time_limit(0);
    add starting variable $x for checking of prime numbers between x and y.

--- get_all_prime.php
+++ get_all_prime.php
@@ -1,7 +1,10 @@
 <?php
 
-function get_all_prime($limit) {
-
+function get_all_prime($x, $limit) {
+	
+  $sum_of_primes=0;
+  set_time_limit(0);
+  
   for($x=1; $x<=$limit; $x++) {
   	$counter=0;
     for($y=1; $y<=$x; $y++) {
@@ -12,11 +15,13 @@ function get_all_prime($limit) {
 	
 	if($counter==2) {
 		echo $x . "\n";
+		$sum_of_primes = $sum_of_primes + $x;
 	}
   }
+  echo "\nThe sum of all primes under $limit is $sum_of_all_primes";
  }
   
-  get_all_prime(1000);
+  get_all_prime(1000000);
 
 
 
