7036a7c1d0a73d60e876b8922b01a0a4e8764ea9
Jowett Isaiah Go <jowettgo@gmail.com>
2015-02-03 17:18:46 -0800

    Update get_lcm_integer.php

--- get_lcm_integer.php
+++ get_lcm_integer.php
@@ -1,4 +1,18 @@
 <?php
 
-
+function get_lcm_integet($number1, $number2) {
+  if !ctype_digit($number1) {
+    echo $number1;
+    echo " does not consist of all digits"
+  }
+  
+  if !(ctype_digit($number2)) {
+    if !ctype_digit($number2) {
+      echo $numer2;
+      echo " does not consist of all digits"
+    }
+    
+  }
+  
+}
 ?>
